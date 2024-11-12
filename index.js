b897f87b829711838d36417bd2815f861b4eb33a
jankidas.parashar@royalmail.com <Application.Devops@royalmail.com>
2024-11-12 17:16:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+require('dotenv').config();
+const screenshot = require('screenshot-desktop');
+const notifier = require('node-notifier');
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+const { GoogleAIFileManager } = require("@google/generative-ai/server");
+
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+const fileManager = new GoogleAIFileManager(apiKey);
+
+/**
+ * Uploads the given file to Gemini.
+ *
+ * See https://ai.google.dev/gemini-api/docs/prompting_with_media
+ */
+async function uploadToGemini(path, mimeType) {
+  const uploadResult = await fileManager.uploadFile(path, {
+    mimeType,
+    displayName: path,
+  });
+  const file = uploadResult.file;
+  console.log(`Uploaded file ${file.displayName} as: ${file.name}`);
+  return file;
+}
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-pro-002",
+  systemInstruction: "Your Job is to answer the question, so please give correct answers only",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run() {
+
+  await captureDesktopScreenshot('./images/quiz.png');
+
+  // TODO Make these files available on the local file system
+  // You may need to update the file paths
+  const files = [
+    await uploadToGemini("./images/quiz.png", "image/png"),
+  ];
+
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+      {
+        role: "user",
+        parts: [
+          {
+            fileData: {
+              mimeType: files[0].mimeType,
+              fileUri: files[0].uri,
+            },
+          },
+        ],
+      },
+    ],
+  });
+
+  const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+  console.log(result.response.text());
+}
+
+async function captureDesktopScreenshot(outputPath) {
+  try {
+    // Capture the screenshot and save it to the specified path
+    await screenshot({ filename: outputPath });
+    console.log(`Screenshot saved at ${outputPath}`);
+
+    // Show a desktop notification
+    notifier.notify({
+      title: 'Screenshot Captured',
+      message: 'Your screenshot has been saved successfully!',
+      sound: true,
+      wait: true
+    });
+
+
+
+  } catch (error) {
+    console.error('Error capturing screenshot:', error);
+  }
+}
+
+
+
+
+run();
+
